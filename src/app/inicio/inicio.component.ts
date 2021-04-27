f40e82f4cb18b8c5607152dfbb0f088f03bd7a4c
flasaflasa <fernandolasa@gmail.com>
2021-04-26 21:13:30 -0300

    1 file modified

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
-  name = 'Angular ';
+  name = 'Angular 4k3 ';
   constructor() { }
 
   ngOnInit() {
